b4521429917b13b6d31485d9bdee7a1ebc06c476
Jaggudda <jayeshkumar171@gmail.com>
2025-03-22 01:32:06 +0530

    CI: implement standard response and error

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+  console.log("MONGODB_connection_failed", error);
+});
 
-// const app = express()
-// (async ()=> {
-// try {
-//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-//     app.on("error", (error) => {
-//         console.error(`Express error: ${error}`)
-//     })
-//     app.listen(process.env.PORT, () => {
-//         console.log(`Server is running on port ${process.env.PORT}`)
-//     }
-// } catch (error) {
-//     console.error(error)
-// }
-// })()
